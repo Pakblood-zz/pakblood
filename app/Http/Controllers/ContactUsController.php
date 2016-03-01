72a29325910e21a9ae4268a7289eaaf5798b6109
Aamir <aamir@aalasolutions.com>
2016-03-01 16:12:36 +0500

    Test commit

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -11,32 +11,34 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Validator;
 
-class ContactUsController extends Controller
-{
-    public function sendMail(Request $request){
+class ContactUsController extends Controller {
+    public function sendMail(Request $request) {
         $data = Input::all();
         $rules = array(
-            'name' => 'required',
-            'email' => 'required|email',
-            'subject' => 'required',
+            'name'                 => 'required',
+            'email'                => 'required|email',
+            'subject'              => 'required',
             'g-recaptcha-response' => 'required|captcha',
-            'message' => 'required',
+            'message'              => 'required',
         );
         $validator = Validator::make($data, $rules);
-        if ($validator->fails()){
+        if ($validator->fails()) {
             return Redirect::to('/contact')->withInput()->withErrors($validator);
         }
-        else{
-            $data = array('name' => $request->name,'email' => $request->email,'subject' => $request->subject
-            ,'msg' => $request->message,);
-            Mail::queue('emails/contact_us', $data, function ($message) use($data) {
+        else {
+            $data = array('name' => $request->name, 'email' => $request->email, 'subject' => $request->subject
+            , 'msg'              => $request->message,);
+            $mail = Mail::send('emails/contact_us', $data, function ($message) use ($data) {
                 $message
-                    ->from($data['email'],$data['name'])
-                    ->to('asad.zaheer@aalasolutions.com')
+//                    ->from($data['email'],$data['name'])
+                    ->to('asad.zaheer@aalasolutions.com','Asad Zaheer')
                     ->subject($data['subject']);
             });
-            return redirect()->back()->with('message','Email has been sent. we will get in touch with you as soon as we can')
-                ->with('type','success');
+            if ($mail) {
+                return redirect()->back()->with('message', 'Email has been sent. we will get in touch with you as soon as we can')
+                    ->with('type', 'success');
+            }
+            dump($mail);
         }
 
     }
