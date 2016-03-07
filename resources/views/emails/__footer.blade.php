5bf8497d6a83970b570969331e0200769b563241
Aamir Mahmood <aamir@aalasolutions.com>
2016-03-07 11:23:20 +0500

    Update profile message

---
+++ resources/views/emails/__footer.blade.php
@@ -0,0 +1,4 @@
+<br><br><br>
+<b>Pakblood</b><br>
+<a href="http://www.pakblood.com">http://www.pakblood.com</a><br>
+<a href="mailto:info@pakblood.com">info@pakblood.com</a>
--- resources/views/emails/profile_updated.blade.php
+++ resources/views/emails/profile_updated.blade.php
@@ -1,16 +1,12 @@
-<!DOCTYPE html>
-<html lang="en-US">
-<head>
-    <meta charset="utf-8">
-</head>
-<body>
-
 <div>
     Dear {{$name}}, <br>
-    Your profile at Pakblood has been updated.Please visit your <a href="pakblood.com">Pakblood</a> account for
-    more details<br>
-    Please contact us at <a href="mailto:info@pakblood.com">info@pakblood.com</a> for any queries.
+    <br>
+    Your profile at Pakblood has been updated.<br>
+    <br>
+    Please visit your <a href="pakblood.com">Pakblood</a> account for
+    more details.
+
+
+    @include ('__footer')
 </div>
 
-</body>
-</html>
